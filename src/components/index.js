36a1845140a3a2f24dc9aff64a8f8fe2a1644718
Swarup <swarupbasu325@gmail.com>
2024-04-12 16:02:13 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button'
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import RTE from './RTE'
+import Select from './Select'
+export {
+    Header,
+    Button,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    Input,
+    RTE,
+    Select,
+}
